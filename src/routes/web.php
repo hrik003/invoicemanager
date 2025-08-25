1c707d3bdd48f37009324ae49d3b68b13958fefa
hrik003 <arijit.wizkraft@gmail.com>
2025-08-25 17:17:08 +0530

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,6 +15,6 @@ Route::get('hello', [WelcomeController::class, 'index']);
 
 Route::middleware(['web','auth'])->group(function () {
     Route::resource('invoices','InvoiceController')->except(['edit','update']); // add as needed
-    Route::get('invoices/{invoice}/download',[InvoiceController::class, 'downloadPdf'])->name('invoices.download');
-    Route::post('invoices/{invoice}/email',[InvoiceController::class, 'emailInvoice'])->name('invoices.email');
+    Route::get('invoices/{invoice}/download','ArnlInvoices\InvoiceManager\Http\Controllers\InvoiceController@downloadPdf');
+    Route::post('invoices/{invoice}/email','ArnlInvoices\InvoiceManager\Http\Controllers\InvoiceController@emailInvoice');
 });
